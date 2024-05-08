7f1bb581b6f69800c6c6f37c8fd18ca4f7e4ec3f
lorenzboss <lorenz.boss@bbzbl-it.ch>
2024-05-08 10:23:29 +0200

    format code with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import AuthForm from './auth-form'
+import AuthForm from "./auth-form";
 
 export default function Home() {
   return (
@@ -6,13 +6,14 @@ export default function Home() {
       <div className="col-6">
         <h1 className="header">Supabase Auth + Storage</h1>
         <p>
-          Experience our Auth and Storage through a simple profile management example. Create a user
-          profile and upload an avatar image. Fast, simple, secure.
+          Experience our Auth and Storage through a simple profile management
+          example. Create a user profile and upload an avatar image. Fast,
+          simple, secure.
         </p>
       </div>
       <div className="col-6 auth-widget">
         <AuthForm />
       </div>
     </div>
-  )
+  );
 }
